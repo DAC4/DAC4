64e411ce4eb745546d27666828c687c851d926d8
Ribesg <ribesg@yahoo.fr>
2014-12-17 16:31:42 +0100

    Servlet test 3

--- src/main/java/imag/dac/dac4/MyServlet.java
+++ src/main/java/imag/dac/dac4/MyServlet.java
@@ -6,27 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
 
 @WebServlet(name = "MyServlet", urlPatterns = "/")
 public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        StringBuilder builder = new StringBuilder();
-        Enumeration attributeNames = req.getAttributeNames();
-        if (attributeNames.hasMoreElements()) {
-            while (attributeNames.hasMoreElements()) {
-                String name = (String) attributeNames.nextElement();
-                builder.append(name).append('=').append(req.getAttribute(name));
-                if (attributeNames.hasMoreElements()) {
-                    builder.append(',');
-                }
-            }
-        } else {
-            builder.append("kek");
-        }
-        req.setAttribute("text", builder.toString());
+        req.setAttribute("text", "kek");
         req.getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 }
