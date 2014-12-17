dea732742a7437c9515ae8226ce43b152ce250f9
Ribesg <ribesg@yahoo.fr>
2014-12-17 16:30:05 +0100

    Servlet test 2

--- src/main/java/imag/dac/dac4/MyServlet.java
+++ src/main/java/imag/dac/dac4/MyServlet.java
@@ -8,19 +8,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Enumeration;
 
-@WebServlet(name = "MyServlet", urlPatterns = "/DAC4")
+@WebServlet(name = "MyServlet", urlPatterns = "/")
 public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         StringBuilder builder = new StringBuilder();
         Enumeration attributeNames = req.getAttributeNames();
-        while (attributeNames.hasMoreElements()) {
-            String name = (String) attributeNames.nextElement();
-            builder.append(name).append('=').append(req.getAttribute(name));
-            if (attributeNames.hasMoreElements()) {
-                builder.append(',');
+        if (attributeNames.hasMoreElements()) {
+            while (attributeNames.hasMoreElements()) {
+                String name = (String) attributeNames.nextElement();
+                builder.append(name).append('=').append(req.getAttribute(name));
+                if (attributeNames.hasMoreElements()) {
+                    builder.append(',');
+                }
             }
+        } else {
+            builder.append("kek");
         }
         req.setAttribute("text", builder.toString());
         req.getRequestDispatcher("/index.jsp").forward(req, resp);
