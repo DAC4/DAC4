a5e8c850efb1688a9621d14a4b43b7742299ef71
Ribesg <ribesg@yahoo.fr>
2014-12-18 14:38:04 +0100

    Quote identifiers

--- sql/reset.sql
+++ sql/reset.sql
@@ -1,49 +1,49 @@
 -- Destroy database if it exists
-DROP DATABASE IF EXISTS troc_box;
+DROP DATABASE IF EXISTS `troc_box`;
 
 -- Database creation and selection
-CREATE DATABASE troc_box;
-USE troc_box;
+CREATE DATABASE `troc_box`;
+USE `troc_box`;
 
 -- Create tables in the database
-CREATE TABLE users
+CREATE TABLE `users`
 (
-  id       VARCHAR(30)  NOT NULL,
-  name     VARCHAR(120) NULL,
-  password VARCHAR(45)  NULL,
-  email    VARCHAR(55)  NULL,
+  `id`       VARCHAR(30)  NOT NULL,
+  `name`     VARCHAR(120) NULL,
+  `password` VARCHAR(45)  NULL,
+  `email`    VARCHAR(55)  NULL,
 
-  PRIMARY KEY (id)
+  PRIMARY KEY (`id`)
 );
 
-CREATE TABLE groups
+CREATE TABLE `groups`
 (
-  id      VARCHAR(30) NOT NULL,
-  user_id VARCHAR(30) NULL,
+  `id`      VARCHAR(30) NOT NULL,
+  `user_id` VARCHAR(30) NULL,
 
-  PRIMARY KEY (id),
-  INDEX USRGPFK00_idx (user_id ASC),
+  PRIMARY KEY (`id`),
+  INDEX `USRGPFK00_idx` (`user_id` ASC),
 
-  CONSTRAINT USRGPFK00 FOREIGN KEY (user_id) REFERENCES users (id)
+  CONSTRAINT `USRGPFK00` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
 
-CREATE TABLE Guest (
-  id   BIGINT      NOT NULL,
-  name VARCHAR(30) NULL,
+CREATE TABLE `guests` (
+  `id`   BIGINT      NOT NULL,
+  `name` VARCHAR(30) NULL,
 
-  PRIMARY KEY (id)
+  PRIMARY KEY (`id`)
 );
 
 -- Insert base values
-INSERT INTO users VALUES ('admin', 'Mr Admin', 'admin', 'admin@admin.admin');
-INSERT INTO groups VALUES ('all', 'admin');
+INSERT INTO `users` VALUES ('admin', 'Mr Admin', 'admin', 'admin@admin.admin');
+INSERT INTO `groups` VALUES ('all', 'admin');
 
-INSERT INTO Guest VALUES (1, 'Toto');
-INSERT INTO Guest VALUES (2, 'Yann');
-INSERT INTO Guest VALUES (3, 'Yann aussi');
-INSERT INTO Guest VALUES (4, 'Yann encore');
-INSERT INTO Guest VALUES (5, 'Yannou');
-INSERT INTO Guest VALUES (6, 'Yhahn');
+INSERT INTO `guests` VALUES (1, 'Toto');
+INSERT INTO `guests` VALUES (2, 'Yann');
+INSERT INTO `guests` VALUES (3, 'Yann aussi');
+INSERT INTO `guests` VALUES (4, 'Yann encore');
+INSERT INTO `guests` VALUES (5, 'Yannou');
+INSERT INTO `guests` VALUES (6, 'Yhahn');
 
 -- Commit
 COMMIT;
