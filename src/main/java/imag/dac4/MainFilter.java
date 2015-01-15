5b1b6b1a671761c08e32456e25a5d3cacf148e28
Ribesg <ribesg@yahoo.fr>
2015-01-15 15:36:00 +0100

    Filter!

---
+++ src/main/java/imag/dac4/MainFilter.java
@@ -0,0 +1,32 @@
+package imag.dac4;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(filterName = "MainFilter", urlPatterns = "/")
+public class MainFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filterChain) throws IOException, ServletException {
+        final HttpServletRequest httpReq = (HttpServletRequest) req;
+        final String path = httpReq.getRequestURI().substring(httpReq.getContextPath().length());
+
+        if (path.startsWith("/css") || path.startsWith("/js")) {
+            // Pass to default servlet
+            filterChain.doFilter(httpReq, resp);
+        } else {
+            // Pass to our front servlet
+            httpReq.getRequestDispatcher("/index").forward(httpReq, resp);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
