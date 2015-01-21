06a74ae737992ddc03ac345e3978eef863a36d21
Tom Andre-Poyaud <tom.andre-poyaud@ensimag.imag.fr>
2015-01-21 14:01:49 +0100

    try

--- src/main/java/imag/dac4/servlet/UserServlet.java
+++ src/main/java/imag/dac4/servlet/UserServlet.java
@@ -1,6 +1,7 @@
 package imag.dac4.servlet;
 
 import imag.dac4.Constants;
+import imag.dac4.model.item.Item;
 import imag.dac4.model.item.ItemDao;
 import imag.dac4.model.user.User;
 
@@ -14,21 +15,19 @@ import java.io.IOException;
 
 @WebServlet(name = "UserServlet", urlPatterns = {
         "/user",
-        "/user/items"
+        "/user/items",
+        "/user/items/remove"
 })
 public class UserServlet extends HttpServlet{
 
     @EJB ItemDao itemDao;
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final Boolean isAdminParam = (Boolean) req.getSession().getAttribute("isAdmin");
         final User user = (User) req.getSession().getAttribute("user");
-        final boolean isAdmin = isAdminParam != null && isAdminParam;
-
         final String[] split = req.getRequestURI().split("/");
         final String action = split[split.length - 1];
+
         switch (action.toLowerCase()) {
             case "items":
-                req.setAttribute("isAdmin", isAdmin);
                 req.setAttribute("items", this.itemDao.getItems(user));
                 req.getRequestDispatcher(Constants.JSP_USER_ITEMS).forward(req, resp);
                 break;
@@ -44,12 +43,39 @@ public class UserServlet extends HttpServlet{
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final String[] split = req.getRequestURI().split("/");
         final String action = split[split.length - 1];
+
         switch (action.toLowerCase()) {
+            case "remove":
+                onItemRemoveRequest(req, resp);
+                break;
             default:
                 req.getSession().setAttribute("error", 400);
                 req.getSession().setAttribute("error_msg", "Bad Request: " + req.getRequestURI());
                 resp.sendRedirect("/");
                 break;
         }
+    }
+
+    private void onItemRemoveRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final User user = (User) req.getSession().getAttribute("user");
+        int id = (int) req.getAttribute("itemId");
+        Item item = itemDao.read(id);
+        if (item != null) {
+            if (item.getOwnerId() == user.getId() || this.isAdmin(req)) {
+                itemDao.delete(id);
+            } else {
+                req.getSession().setAttribute("error", 403);
+                req.getSession().setAttribute("error_msg", "Forbidden: this item is not yours.");
+            }
+        } else {
+            req.getSession().setAttribute("error", 404);
+            req.getSession().setAttribute("error_msg", "Not found: no such item");
+        }
+        req.getRequestDispatcher(Constants.JSP_USER_ITEMS).forward(req, resp);
+    }
+
+    private boolean isAdmin(final HttpServletRequest req) {
+        final Boolean isAdmin = (Boolean) req.getSession().getAttribute("isAdmin");
+        return isAdmin != null && isAdmin;
     }
 }
