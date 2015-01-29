fa156c7720edfb58a1ef7674946c81d21fab96fe
Ribesg <ribesg@yahoo.fr>
2015-01-29 14:38:06 +0100

    .

--- src/main/java/imag/dac4/model/config/ConfigDao.java
+++ src/main/java/imag/dac4/model/config/ConfigDao.java
@@ -5,16 +5,16 @@ import imag.dac4.model.Dao;
 import javax.ejb.Stateless;
 
 @Stateless
-public class ConfigDao extends Dao<String, String> {
+public class ConfigDao extends Dao<String, Config> {
 
     public ConfigDao() {
-        super(String.class);
+        super(Config.class);
     }
 
     public Integer getLockerAmount() {
         try {
-            return Integer.parseInt(this.read("lockerAmount"));
-        } catch (final NumberFormatException e) {
+            return Integer.parseInt(this.read("lockerAmount").getValue());
+        } catch (final NullPointerException | NumberFormatException e) {
             return null;
         }
     }
