b70f1dae4bc41fe6c646f471813f88525871d87a
Yann Laforest <laforest.yann@gmail.com>
2015-01-30 09:56:39 +0100

    Correction again

--- src/test/java/imag/dac4/selenium/ReturnItem.java
+++ src/test/java/imag/dac4/selenium/ReturnItem.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.Select;
 
 public class ReturnItem {
 
+  @Test
   public void testReturnItem() throws Exception {
 
     final WebDriver driver = TestSuiteSelenium.getDriver();
